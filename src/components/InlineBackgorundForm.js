8047da4bf6df422483cc7f88015a308ce47285a0
Kaj Białas <kaj.bialas@dev-industry.com>
2020-11-21 10:10:00 +0100

    extra issue1

--- src/components/InlineBackgorundForm.js
+++ src/components/InlineBackgorundForm.js
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 
 function InlineBackgroundForm() {
   const [ color, setColor ] = useState('');
+  const [ hover, setHover ] = useState(false);
 
   const handleChange = (e) => setColor(e.target.value);
+  const handleHoverOn = () => setHover(true);
+  const handleHoverOff = () => setHover(false);
 
   const inlineBackgroundFormStyle = {
-    backgroundColor: color,
+    backgroundColor: hover ? 'green' : color ? color : 'white',
   }
 
   return (
-    <div style={inlineBackgroundFormStyle}>
+    <div style={inlineBackgroundFormStyle} onMouseEnter={handleHoverOn} onMouseLeave={handleHoverOff}>
       <input type="text" value={color} onChange={handleChange} />
     </div>
   )
