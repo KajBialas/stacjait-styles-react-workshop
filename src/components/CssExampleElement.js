0728ed38cdeb7f5ccf325c65a87a42fd03e8137d
Kaj Białas <kaj.bialas@dev-industry.com>
2020-11-21 10:55:52 +0100

    extra issue 2

--- src/components/CssExampleElement.js
+++ src/components/CssExampleElement.js
@@ -3,12 +3,22 @@ import './cssExampleElement.css';
 
 function CssExampleElement() {
   const [ active, setActive ] = useState(false);
+  const [ color, setColor ] = useState('');
 
   const handleClick = () => setActive(!active);
+  const handleChange = (e) => setColor(e.target.value);
+
+  const cssExampleElementContainerStyle = {
+    backgroundColor: color,
+  }
 
   return (
-    <div className={active ? 'cssExampleElementContainer' : 'cssExampleElementContainerActive'}>
+    <div
+      className={active ? 'cssExampleElementContainer' : 'cssExampleElementContainerActive'}
+      style={cssExampleElementContainerStyle}
+    >
       <button className="cssExampleElementButton" onClick={handleClick}>Change color</button>
+      <input type="text" value={color} onChange={handleChange} />
     </div>
   )
 }
