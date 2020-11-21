40f1c6ae5b66bbd708806e4d6d7b83002fe8932e
Kaj Białas <kaj.bialas@dev-industry.com>
2020-11-21 14:47:10 +0100

    better solution for issue6

--- src/components/exampleForm/ExampleForm.js
+++ src/components/exampleForm/ExampleForm.js
@@ -1,28 +1,14 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  height: 500px;
-  background-color: ${({color}) => color ? color : 'blue'};
-  
-  &:hover {
-    background-color: green;
-  }
-`;
-
-const Input = styled.input`
-  border: 1px solid red;
-  font-size: 20px;
-`;
+import { StyledContainer, StyledInput } from './style';
 
 function ExampleForm() {
   const [ color, setColor ] = useState('');
 
   const handleColor = e => setColor(e.target.value);
   return (
-    <Container color={color}>
-      <Input value={color} onChange={handleColor} />
-    </Container>
+    <StyledContainer color={color}>
+      <StyledInput value={color} onChange={handleColor} />
+    </StyledContainer>
   )
 };
 
