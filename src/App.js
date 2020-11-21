32923076638316846a9db31697185c8e6ee96fd5
Kaj Białas <kaj.bialas@dev-industry.com>
2020-11-21 09:38:14 +0100

    init issue1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React, { useState } from 'react';
+
+const ContainerStyle = {
+  color: 'red',
+  backgroundColor: 'blue',
+}
+
+const HeaderStyle = {
+  fontSize: '20px',
+}
 
 function App() {
+  const [ color, setColor ] = useState(false);
+
+  const handleClick = () => setColor(!color);
+
+  const headerStyle = {
+    ...HeaderStyle,
+    backgroundColor: color ? 'green' : 'yellow',
+  }
+
   return (
-    <div>Aplikacja</div>
+    <div style={ContainerStyle}>
+      <h1 style={headerStyle}>Aplikacja</h1>
+      <button onClick={handleClick}>Change color</button>
+    </div>
   );
 }
 
